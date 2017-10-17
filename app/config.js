9ed4d7ec73570edf756088a541c8dd8c26e6ec61
Sebastian Limbach <bastilimbach@gmail.com>
2017-10-17 16:53:12 +0200

    Add MONGODB_URL env variable

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ module.exports = {
     port: process.env.PORT || 3000,
   },
   db: {
-    url: 'mongodb://mongodb/',
+    url: process.env.MONGODB_URL || 'mongodb://localhost/', // mongodb_manager - tAB6YHzI'DGCndv~'f&LM7nXqImZtJr{JW7'2S9$rgHGYKeDn29(FR4cy2WwkwPV
     mongoDBConfig: {
       useMongoClient: true,
       socketTimeoutMS: 0,
