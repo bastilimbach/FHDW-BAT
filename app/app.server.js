16ed6e504c34786f7a92d9d3bdd50fcfcb52e7c9
Sebastian Limbach <bastilimbach@gmail.com>
2017-11-09 13:50:39 +0100

    Add static files (homepage)

--- app/app.server.js
+++ app/app.server.js
@@ -7,43 +7,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bearerToken())
-app.get('/', (req, res) => {
-  res.status(200)
-  res.json({
-    success: true,
-    message: 'The API is running successfully!',
-    routes: [
-      {
-        path: '/user',
-        methods: {
-          get: 'Get a list of all users.',
-          post: 'Add a new user.',
-          put: null,
-          delete: null,
-        },
-      },
-      {
-        path: '/user/[user_id]',
-        methods: {
-          get: 'Get user information.',
-          post: null,
-          put: 'Update user information.',
-          delete: 'Delete user.',
-        },
-      },
-      {
-        path: '/user/[user_id]/location',
-        methods: {
-          get: 'Get current user location.',
-          post: null,
-          put: 'Update current user location.',
-          delete: null,
-        },
-      },
-    ],
-  })
-})
-
+app.use('/', express.static('./public'))
 app.use('/user', routes)
 
 module.exports = app
