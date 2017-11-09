799abd66506441736b9e3df7678e10f767e1846b
Sebastian Limbach <bastilimbach@gmail.com>
2017-11-09 13:51:26 +0100

    Add env variables for database url & admin token

--- app/config.js
+++ app/config.js
@@ -3,9 +3,7 @@ module.exports = {
     port: process.env.PORT || 3000,
   },
   db: {
-    // url: process.env.MONGODB_URL || 'mongodb://localhost/', // mongodb_manager - tAB6YHzI'DGCndv~'f&LM7nXqImZtJr{JW7'2S9$rgHGYKeDn29(FR4cy2WwkwPV
-    url: 'mongodb://mongodb_manager:tAB6YHzI\'DGCndv%7E\'f&LM7nXqImZtJr%7BJW7\'2S9%24rgHGYKeDn29(FR4cy2WwkwPV@35.197.211.150/admin?replicaSet=rs0',
-    // url: 'mongodb://35.197.211.150/admin',
+    url: process.env.MONGODB_URL || 'mongodb://localhost/',
     mongoDBConfig: {
       useMongoClient: true,
       socketTimeoutMS: 0,
@@ -13,5 +11,5 @@ module.exports = {
       reconnectTries: 30,
     },
   },
-  adminToken: 'sebastian',
+  adminToken: process.env.API_ADMIN_TOKEN || 'sebastian',
 }
