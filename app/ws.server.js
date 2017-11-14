61c3a4f7e7cd6740d90151f03c8bacdef2b9f033
TobiCCDev <t.reim@chargecloud.de>
2017-11-14 12:36:43 +0100

    add socket cert

--- app/ws.server.js
+++ app/ws.server.js
@@ -1,12 +1,17 @@
 const WebSocket = require('ws')
 
 function createServer() {
-  const wsServer = new WebSocket.Server({ port: 8080 })
+  const wsServer = new WebSocket.Server({
+    key: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/privkey.pem`),
+    cert: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/cert.pem`),
+    ca: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/chain.pem`),
+    port: 8080
+  })
   wsServer.on('connection', (ws) => {
     wsServer.client = ws
   })
   wsServer.sendMsg = function sendMsg(msg) {
-    if (wsServer.client !== undefined && wsServer.client.readyState === WebSocket.OPEN)wsServer.client.send(msg)
+    if (wsServer.client !== undefined && wsServer.client.readyState === WebSocket.OPEN) wsServer.client.send(msg)
   }
   return wsServer
 }
