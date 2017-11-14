5f06fe6876917ff9d497db0b000902ef905440f9
TobiCCDev <t.reim@chargecloud.de>
2017-11-14 12:48:48 +0100

    changed ws opening

--- app/ws.server.js
+++ app/ws.server.js
@@ -1,12 +1,20 @@
 const WebSocket = require('ws')
+const https = require('https')
 
 function createServer() {
-  const wsServer = new WebSocket.Server({
-    key: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/privkey.pem`),
-    cert: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/cert.pem`),
-    ca: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/chain.pem`),
-    port: 8080
-  })
+  const wsServer
+  if (process.env.NODE_ENV === 'production') {
+    const serverOptions = {
+      key: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/privkey.pem`),
+      cert: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/cert.pem`),
+      ca: fs.readFileSync(`/certs/letsencrypt/live/${config.server.domain}/chain.pem`),
+      port: 8080,
+    }
+    wsServer = new WebSocket.Server(https.createServer(serverOptions))
+  } else {
+    wsServer = new WebSocket.Server({port:8080})
+  }
+  
   wsServer.on('connection', (ws) => {
     wsServer.client = ws
   })
