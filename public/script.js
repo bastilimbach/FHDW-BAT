da48d34386da42c6cb9a21ff2eef47b7e809bbd4
Sebastian Limbach <bastilimbach@gmail.com>
2017-11-14 17:12:07 +0100

    Add websocket data parsing

--- public/script.js
+++ public/script.js
@@ -1,65 +1,69 @@
-var markerCoordinates = {lat: 50.98382, lng: 7.1174013};
-var marker;
-var map;
-var directionsService;
+/* eslint-disable */
+const markerCoordinates = { lat: 50.98382, lng: 7.1174013 }
+let marker
+let map
+let directionsService
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: markerCoordinates,
-		disableDefaultUI: true
-	});
-	marker = new google.maps.Marker({
-		position: markerCoordinates,
-		map: map
-	});
-	directionsService = new google.maps.DirectionsService;
-	
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: markerCoordinates,
+    disableDefaultUI: true,
+  })
+  marker = new google.maps.Marker({
+    position: markerCoordinates,
+    map,
+  })
+  directionsService = new google.maps.DirectionsService()
+
 }
 
-function moveMarker(lat, lng){
-	marker.setPosition( new google.maps.LatLng( lat, lng ) );
-	map.panTo( new google.maps.LatLng( lat, lng ) );
+function moveMarker(lat, lng) {
+  marker.setPosition(new google.maps.LatLng(lat, lng))
+  map.panTo(new google.maps.LatLng(lat, lng))
 }
 
 function displayMessage(msg) {
-	var messageEl = document.getElementById("userMessage");
-	messageEl.innerHTML = msg;
+  const messageEl = document.getElementById('userMessage')
+  messageEl.innerHTML = msg
 }
 
 function getDuration(origin, destination, callback) {
-	directionsService.route({
-		origin: new google.maps.LatLng( origin[0], origin[1] ),
-		destination: new google.maps.LatLng( destination[0], destination[1] ),
-		travelMode: 'DRIVING'
-	}, function(response, status) {
-		if (status === 'OK') {
-			callback(response.routes[0].legs[0].duration.text);
-		} else {
-			callback("Error");
-		}
-	});
+  directionsService.route({
+    origin: new google.maps.LatLng(origin[0], origin[1]),
+    destination: new google.maps.LatLng(destination[0], destination[1]),
+    travelMode: 'DRIVING',
+  }, (response, status) => {
+    if (status === 'OK') {
+      callback(response.routes[0].legs[0].duration.text)
+    } else {
+      callback('Error')
+    }
+  })
 }
 
-var ws = new WebSocket('ws://localhost:8080')
+const ws = new WebSocket('ws://localhost:3000')
 ws.onmessage = function (event) {
-	console.log(event.data)
-	if(event.data.header === "Message") {
-		displayMessage(event.data);
-	} else if(event.data.header === "Location") {
-		var lat = event.data.latitude;
-		var lng = event.data.longitude;
-		moveMarker(lat, lng);
-		if(event.data.destination !== "null"){
-			getDuration([lat,lng], [event.data.destination.lat,event.data.destination.lng], function(time) {
-				document.getElementById("time").innerHTML = time;
-			});
-		} else {
-			document.getElementById("time").innerHTML = "Hat kein Ziel";
-		}
-	} 
+  const wsData = JSON.parse(event.data)
+  switch (wsData.type) {
+    case 'location':
+      const lat = wsData.location.latitude
+      const lng = wsData.location.latitude
+      moveMarker(lat, lng)
+      if (wsData.destination !== 'null' && typeof wsData.destination !== 'undefined') {
+        getDuration([lat, lng], [wsData.destination.latitude, wsData.destination.longitude], (time) => {
+          document.getElementById('time').innerHTML = time
+        })
+      } else {
+        document.getElementById('time').innerHTML = 'Hat kein Ziel'
+      }
+      break
+    case 'message':
+      displayMessage(wsData.message)
+      break
+    default:
+      break
+  }
 }
 ws.onopen = function (event) {
-	alert("Socket connected");
+//   alert('Socket connected')
 }
-  
-//https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyA830pjkq_FQY1MkDCpShsfxLTy3ALZBQA
